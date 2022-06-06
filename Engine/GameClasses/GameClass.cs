4c8fd02cb6237619a4812566ed2c2d044faffce0
Stuart Nolton <stuart@growflow.com>
2022-06-06 01:15:29 -0400

    starting to do class stuff

---
+++ Engine/GameClasses/GameClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Text_Based_Adventure.Engine.GameTools;
+
+namespace Text_Based_Adventure.Engine.GameClasses
+{
+    public enum Proficiencies
+    {
+        LightArmor,
+        MediumArmor,
+        Shields,
+        SimpleWeapons,
+        MartialWeapons
+    }
+    public class GameClass
+    {
+        int ProficencyBonus; // Current Proficency Bonus
+        DiceSet HitDice;
+        int Health;
+        List<Proficiencies> Proficiencies;
+    }
+}
