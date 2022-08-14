378c2cdf00dd3e37702d46b66a7ee75fa6aa0033
Stuart Nolton <stuart@growflow.com>
2022-08-14 01:37:13 -0400

    branking out into other skill checks

--- Engine/GameObjects/SkillChecks/SkillCheck.cs
+++ Engine/GameObjects/SkillChecks/SkillCheck.cs
@@ -14,11 +14,13 @@ namespace Text_Based_Adventure.Engine.GameObjects.SkillChecks
 
         public bool Broken; // When true skill check can no longer be attempted or occur
 
+        // how hard is this thing?
         public int BestTarget;
         public int GoodTarget;
         public int BadTarget;
 
-        public AttributeSet Attributes; // The Attributes checks you need to pass for this skill check (can be a combo)
+        public Attribute Attribute; // The Attributes checks you need to pass for this skill check (can be a combo)
+        public Skill Skill; // The skill need to pass the skill check.
 
         // The action that cause the skill check to happen
         // The thing you are trying to do  For passive skills it happens automatically
@@ -29,12 +31,16 @@ namespace Text_Based_Adventure.Engine.GameObjects.SkillChecks
 
         public int PerformSkillCheck(PlayerObject player, Attribute attribute)
         {
-            int target = this.Attributes.getAttribute(attribute);
             int attempt = player.getFullMod(attribute) + Util.d20();
-            Util.log($"Attribute Check for {attribute} need a {target} or above");
+            Util.log($"Attribute Check for {attribute} need a {GoodTarget} or above");
             Util.log($"Player Total {attempt}");
-            return attempt - target;
+            return attempt;
+        }
 
+        // SKill is implied
+        public int PerformSkillCheck(PlayerObject player) 
+        {
+            return 0;
         }
     }
 }
---
+++ Engine/UserInputs/GameActions/SkillActions/PickLock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Text_Based_Adventure.GameObjects;
+
+namespace Text_Based_Adventure.Engine.UserInputs.GameActions.SkillActions
+{
+    public class PickLock : SkillAction
+    {
+        public override List<string> keyWord => new List<string>() { "pick", "lockpick", "picklock" };
+
+        public override int duration => 2;
+
+        public override void BadOutcome(GameController? gameController, GameObject? directObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void BestOutcome(GameController? gameController, GameObject? directObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GoodOutcome(GameController? gameController, GameObject? directObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string HelpText()
+        {
+            return "You can attempt to pick a locked item such as a door or chest." +
+                "type 'pick <itemName>' to give it a try";
+        }
+
+        public override void WorstOutcome(GameController? gameController, GameObject? directObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
