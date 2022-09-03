e640f6e00f708ee28b6de1e151e593d7fe18cd8a
Stuart Nolton <switch201@yahoo.com>
2022-09-03 15:53:11 -0400

    Ok going to break things now

---
+++ Content/TestLevel/Scripts/Traps/PoisonDartTrap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Text_Based_Adventure.Engine.Scripts;
+
+namespace Text_Based_Adventure.Content.TestLevel.Scripts.Traps
+{
+    public class PoisonDartTrap : Script
+    {
+        public override void Execute()
+        {
+            
+        }
+    }
+}
---
+++ Engine/Levels/Level.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_Based_Adventure.Engine.Levels
+{
+    public class Level
+    {
+        
+    }
+}
