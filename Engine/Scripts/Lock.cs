6998f14e30a028bdd6458a139158174d218f1730
Stuart Nolton <switch201@yahoo.com>
2022-09-10 23:27:18 -0400

    WIP

---
+++ Engine/Scripts/Lock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Text_Based_Adventure.GameObjects;
+
+namespace Text_Based_Adventure.Engine.Scripts
+{
+    internal class Lock : Script
+    {
+        public override void onMessage(GameObject self, string messageId, string message, GameObject sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameObject self)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
